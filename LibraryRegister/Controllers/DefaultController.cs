e4326bb98cd56f3a6d831025e21707052a62b65d
Barış Bükümcüler <barisbukumculer@gmail.com>
2024-03-21 15:21:02 +0300

    Add project files.

---
+++ LibraryRegister/Controllers/DefaultController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LibraryRegister.Models.Entity;
+
+namespace LibraryRegister.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        LibraryDbEntities db=new LibraryDbEntities();
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult AddUser()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddUser(TBLKAYIT p1)
+        {
+            db.TBLKAYIT.Add(p1);
+            db.SaveChanges();
+            return View();
+        }
+    }
+}
---
+++ LibraryRegister/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LibraryRegister.MvcApplication" Language="C#" %>
